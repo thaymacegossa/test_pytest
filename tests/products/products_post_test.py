57fffe2226dc99702f7a0c1bba4e8530303a2466
Simon Lucena <thaymacegossa@gmail.com>
2026-06-16 11:41:48 -0300

    fix/products_post_test
    - Inclusão de Auth;
    - Remoção de "categoria";
    - Ajuste no assert da mensagem de erro.

--- tests/products/products_post_test.py
+++ tests/products/products_post_test.py
@@ -9,14 +9,13 @@ def _generate_product_payload():
         "preco": 199,
         "descricao": "Descrição de teste",
         "quantidade": 10,
-        "categoria": "Eletrônicos"
     }
 
 
 # test scenario 1: create product successfully
-def test_create_product_successfully(base_url):
+def test_create_product_successfully(base_url, auth_headers):
     payload = _generate_product_payload()
-    response = requests.post(f"{base_url}/produtos", json=payload)
+    response = requests.post(f"{base_url}/produtos", json=payload, headers=auth_headers)
 
     assert response.status_code in (200, 201)
 
@@ -27,10 +26,12 @@ def test_create_product_successfully(base_url):
 
 
 # test scenario 2: create product missing required fields returns error
-def test_create_product_missing_required_fields_returns_error(base_url):
-    response = requests.post(f"{base_url}/produtos", json={"nome": "Produto Incompleto"})
+def test_create_product_missing_required_fields_returns_error(base_url, auth_headers):
+    response = requests.post(f"{base_url}/produtos", json={"nome": "Produto Incompleto"}, headers=auth_headers)
 
     assert response.status_code == 400
 
     response_data = response.json()
-    assert "message" in response_data
+    assert "descricao é obrigatório" in response_data.get("descricao")
+    assert "preco é obrigatório" in response_data.get("preco")
+    assert "quantidade é obrigatório" in response_data.get("quantidade")
